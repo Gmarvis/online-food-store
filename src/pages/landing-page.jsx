798f6cf3fa2f49066895f03d06c9cda9d15d2834
gmarvis <106551910+Gmarvis@users.noreply.github.com>
2023-06-12 20:47:15 +0100

    Revert "Feature/admin dashboard"

--- src/pages/landing-page.jsx
+++ src/pages/landing-page.jsx
@@ -1,52 +1,39 @@
-import {useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useFooditerms } from "../component/context";
+import React, { useContext } from "react";
+import { FoodContext } from "../component/context";
+import { useState } from "react";
 
 export const LandingPage = () => {
-  const [product, setProduct] = useState()
-  const { value } = useFooditerms();
-  const navigate = useNavigate();
+    const { foodItems } = useContext(FoodContext);
+    const [purchase, setPurchase] = useState([foodItems])
 
-  useEffect(() => {
-    setProduct(value);
-  },[]);
 
-  return (
-    <>
-      <div className="landingHeader">
-        <h1 className="mainText">Food Market Place</h1>
-      </div>
+    const handlePurchase = () => {
 
-      <div className="foodCards ">
-        {product?.map((foodItem) => {
-          // handle click navigation to product detail page.
-          const handDitails = () => {
-            navigate(`./products/${foodItem.name}`);
-          };
-          return (
-            <div
-              key={foodItem.name}
-              className="foodItem border border-yellow-700  h-1/4 p-2 text-center rounded justify-center items-center object-scale-down"
-            >
-              <img
-                src={foodItem.image}
-                alt={foodItem.name}
-                className="card h-24 items-center mx-auto"
-              />
-              <h3 className="fItern text-yellow-600">{foodItem.name}</h3>
-              <span>${foodItem.price}</span>
-              <div>
-                <button
-                  className="btn border border-yellow-700 text-yellow-700 p-1  hover:text-yellow-700 hover:bg-white"
-                  onClick={handDitails}
-                >
-                  Detials
-                </button>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </>
-  );
-};
+    }
+
+
+    return (
+        <div className="foodCards flex flex-wrap justify-between gap-10 mt-3">
+            {foodItems.map((foodItem) => {
+
+                return (
+                    <div key={foodItem.id} className="foodItem border border-yellow-700 w-1/5 h-1/4 p-2 text-center rounded" >
+                        <img src={foodItem.image} alt={foodItem.name} className="card h-24" />
+                        <h3 className="fItern text-yellow-600">{foodItem.name}</h3>
+
+                        {/* <p>{foodItem.description}</p> */}
+
+                        <span>${foodItem.price}</span>
+
+                        <br></br>
+                        <button className="btn border border-yellow-700 text-white rounded p-1 bg-yellow-700 hover:text-yellow-700 hover:bg-white" onClick={handlePurchase}>Detials</button>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+
+
+
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,52 +1,14 @@
-import React, { useEffect } from "react";
-import { useContext } from "react";
-import { useParams } from "react-router-dom";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useFooditerms } from "../component/context";
+import React from "react";
+import { FoodContext } from "../component/context";
+// import { Nav } from "../component/nav";
 
-export const Product = () => {
-  // const [foodDetail, setFoodDetail] = useState({});
-  const { value } = useFooditerms();
-  // console.log("foodItems", foodItems);
-
-  const navigate = useNavigate();
-  const params = useParams();
-  // console.log("THis is our", params.name);
-
-  const handlePurcase = () => {
-    // navigate('/login')
- 
-      navigate(`/login/${params.name}`);
-  
-  };
-
-const [foodDetail] = value.filter((food) => food.name === params.name);
-console.log(foodDetail)
-
-
-  console.log("foodDetail ", foodDetail);
-
-  return (
-    <div className="pDetail border mt-5 border-yellow-700 p-2 w-fit m-auto">
-      <h1 className="hd text-xl text-yellow-700">Food Details</h1>
-      <div>
-        <img
-          className="fdetails w-80 "
-          src={foodDetail.image}
-          alt={foodDetail.name}
-        />
-        <h1>{foodDetail.name}</h1>
-        <p>{foodDetail.detials}</p>
-        <span>${foodDetail.price}</span>
-        <br></br>
-        <button
-          className="btn border border-yellow-700 rounded p-1 text-yellow-700 bg-white"
-          onClick={handlePurcase}
-        >
-          Purchase
-        </button>
-      </div>
-    </div>
-  );
-};
+export const Product = ()=>{
+    return (
+        <div>
+        <div className="main h-screen">
+            <h1 className="main-text text-3xl text-center text-yellow-700">Food Details</h1>
+            <h2>Selected Food Iterms</h2>
+        </div>
+        </div>
+    )
+}
